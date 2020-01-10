f746128fc7a63a4f71192d2e0a7a2d48f4ef9a5c
KerronKing <king_of_palmiste@hotmail.com>
2020-01-10 17:48:19 -0400

    Add methods and logic to change display temp from celcius to fahrenheit

--- src/api.js
+++ src/api.js
@@ -1,18 +1,19 @@
 import page from './dom.js'
 const external = (() => {
-  const open = async (city, country) => {
+  const open = async (city, countryCode, countryName) => {
     const apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=';
     const appID = '6afc4c4d6d160273a7559007309c6525';
-    const response = await fetch(`${apiCall}${city},${country}&APPID=${appID}`, { mode: 'cors' });
+    const response = await fetch(`${apiCall}${city},${countryCode}&APPID=${appID}`, { mode: 'cors' });
     const weatherData = await response.json();
-    page.render(weatherData);
+    page.render(weatherData, countryName);
+    page.tempChanger(weatherData);
     console.log(`${weatherData.name}, ${weatherData.main['temp']}, ${weatherData.weather[0].description}`);
   };
   const getWeather = async (city, country) => {
     const internal = await fetch('/src/names.json', { mode: 'cors' });
     const code = await internal.json();
     const cCode = await Object.keys(code).find(key => code[key] === `${country}`).toLowerCase();
-    open(city, cCode);
+    open(city, cCode, country);
   };
   return { getWeather };
 })();
--- src/dom.js
+++ src/dom.js
@@ -1,26 +1,41 @@
 const page = (() => {
-  const render = (obj) => {
+  const render = (obj, name) => {
     const cityLabel = document.getElementById('city-label');
-    cityLabel.textContent = `${obj.name}`;
-    const conditions = document.getElementById('conditions');
-    conditions.textContent = `${obj.main['temp']}`;
+    cityLabel.textContent = `${obj.name} (${name})`;
     const temperature = document.getElementById('temperature');
-    temperature.textContent = `${obj.weather[0].description}`;
-
-    if (temperature.textContent.includes('overcast')) {
+    const celTemp = Math.ceil(obj.main['temp'] - 273);
+    temperature.textContent = `${celTemp} deg C`;
+    const conditions = document.getElementById('conditions');
+    conditions.textContent = `${obj.weather[0].description}`;
+    if (conditions.textContent.includes('overcast')) {
       document.body.id = '';
       document.body.id = 'overcast';
-    } else if (temperature.textContent.includes('clear')) {
+    } else if (conditions.textContent.includes('clear')) {
       document.body.id = '';
       document.body.id = 'sunny';
-    } else if (temperature.textContent.includes('rain')) {
+    } else if (conditions.textContent.includes('rain')) {
       document.body.id = '';
       document.body.id = 'rainy';
-    } else if (temperature.textContent.includes('clouds')) {
+    } else if (conditions.textContent.includes('clouds')) {
       document.body.id = '';
       document.body.id = 'clear';
     }
   }
-  return { render }
+  const tempChanger = (obj) => {
+    const temp = document.getElementById('temp');
+    const temperature = document.getElementById('temperature');
+    temp.addEventListener('change', () => {
+      if (temp.checked) {
+        temperature.textContent = '';
+        const fhrTemp = Math.ceil((obj.main['temp'] - 273) * (9/5) + 32);
+        temperature.textContent = `${fhrTemp} deg F`;
+      } else {
+        temperature.textContent = '';
+        const celTemp = Math.ceil(obj.main['temp'] - 273);
+        temperature.textContent = `${celTemp} deg C`;
+      }
+    });
+  }
+  return { render, tempChanger }
 })();
 export default page;
