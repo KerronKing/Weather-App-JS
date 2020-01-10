8840877e2fb45584c445e2481baad460dfbe680b
KerronKing <king_of_palmiste@hotmail.com>
2020-01-10 13:04:02 -0400

    Add event listener to index.js and refactor API logic

--- src/api.js
+++ src/api.js
@@ -1,21 +1,18 @@
-import form from './form';
-
 const external = (() => {
-  const getCountryCode = async () => {
-    const internal = await fetch('src/names.json', { mode: 'cors' });
-    const code = await internal.json();
-    return Object.keys(code).find(key => code[key] === `${form.getInput().country}`).toLowerCase();
-  };
-  const weatherGen = async () => {
-    const city = form.getInput().city;
-    const countryCode = getCountryCode();
+  const open = async (city, country) => {
     const appID = '6afc4c4d6d160273a7559007309c6525';
-    const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city},
-      ${countryCode}&APPID=${appID}`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${appID}`, { mode: 'cors' });
     const weatherData = await response.json();
     console.log(weatherData);
   };
-  return { weatherGen };
+  const getWeather = async (city, country) => {
+    const internal = await fetch('/src/names.json', { mode: 'cors' });
+    const code = await internal.json();
+    const cCode = await Object.keys(code).find(key => code[key] === `${country}`).toLowerCase();
+    open(city, cCode);
+  };
+  
+  return { getWeather };
 })();
 export default external;
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-/* eslint-disable no-unused-vars */
-import page from './dom.js';
 import external from './api.js';
+
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(form));
+  const city = data['city'];
+  const country = data['country'];
+  external.getWeather(city, country);
+  form.reset();
+});
