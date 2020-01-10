41ea45456333f24b8ce42ea9f199eaed1a34538f
KerronKing <king_of_palmiste@hotmail.com>
2020-01-10 16:26:46 -0400

    Refactor page.render function and edit background styles

--- src/dom.js
+++ src/dom.js
@@ -6,6 +6,20 @@ const page = (() => {
     conditions.textContent = `${obj.main['temp']}`;
     const temperature = document.getElementById('temperature');
     temperature.textContent = `${obj.weather[0].description}`;
+
+    if (temperature.textContent.includes('overcast')) {
+      document.body.id = '';
+      document.body.id = 'overcast';
+    } else if (temperature.textContent.includes('clear')) {
+      document.body.id = '';
+      document.body.id = 'sunny';
+    } else if (temperature.textContent.includes('rain')) {
+      document.body.id = '';
+      document.body.id = 'rainy';
+    } else if (temperature.textContent.includes('clouds')) {
+      document.body.id = '';
+      document.body.id = 'clear';
+    }
   }
   return { render }
 })();
