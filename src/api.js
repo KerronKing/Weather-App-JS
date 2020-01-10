e06cf2129ca64494482e89402ba2f21cccf5dac9
KerronKing <king_of_palmiste@hotmail.com>
2020-01-10 15:41:31 -0400

    Add DOM page.render function and refactor open function

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
+import page from './dom.js'
 const external = (() => {
   const open = async (city, country) => {
+    const apiCall = 'https://api.openweathermap.org/data/2.5/weather?q=';
     const appID = '6afc4c4d6d160273a7559007309c6525';
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${appID}`, { mode: 'cors' });
+    const response = await fetch(`${apiCall}${city},${country}&APPID=${appID}`, { mode: 'cors' });
     const weatherData = await response.json();
-    console.log(weatherData);
+    page.render(weatherData);
+    console.log(`${weatherData.name}, ${weatherData.main['temp']}, ${weatherData.weather[0].description}`);
   };
   const getWeather = async (city, country) => {
     const internal = await fetch('/src/names.json', { mode: 'cors' });
@@ -11,7 +14,6 @@ const external = (() => {
     const cCode = await Object.keys(code).find(key => code[key] === `${country}`).toLowerCase();
     open(city, cCode);
   };
-  
   return { getWeather };
 })();
 export default external;
--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,11 @@
 const page = (() => {
-  const render = () => {
-    
+  const render = (obj) => {
+    const cityLabel = document.getElementById('city-label');
+    cityLabel.textContent = `${obj.name}`;
+    const conditions = document.getElementById('conditions');
+    conditions.textContent = `${obj.main['temp']}`;
+    const temperature = document.getElementById('temperature');
+    temperature.textContent = `${obj.weather[0].description}`;
   }
   return { render }
 })();
