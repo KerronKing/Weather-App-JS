d79db1846efc3a36014503f8351b9a47bdb20597
KerronKing <king_of_palmiste@hotmail.com>
2020-01-14 11:07:47 -0400

    Fix input capitalization issue

--- src/api.js
+++ src/api.js
@@ -9,11 +9,19 @@ const external = (() => {
     page.render(weatherData, countryName);
     page.tempChanger(weatherData);
   };
+  const capitalize = (entry) => {
+    return entry.toLowerCase()
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  };
   const getWeather = async (city, country) => {
+    const casedCountry = capitalize(country);
+    const casedCity = capitalize(city);
     const internal = await fetch('/src/names.json', { mode: 'cors' });
     const code = await internal.json();
-    const cCode = await Object.keys(code).find(key => code[key] === `${country}`).toLowerCase();
-    open(city, cCode, country);
+    const cCode = await Object.keys(code).find(key => code[key] === `${casedCountry}`).toLowerCase();
+    open(casedCity, cCode, casedCountry);
   };
   return { getWeather };
 })();
